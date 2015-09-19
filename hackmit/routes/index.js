9a87feb492dfc1086544e2cf0e182c7e99da8a34
Liang <zhoul@mit.edu>
2015-09-19 14:05:56 -0400

    clarifai js client

--- hackmit/routes/index.js
+++ hackmit/routes/index.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var request = require('resquest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Impression' });
+	res.render('index', { title: 'Impression' });
 });
 
 /* GET test page. */
 router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Test Page' });
+	request.post(
+    'http://www.yoursite.com/formpage',
+    { form: { key: 'value' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
+	res.render('test', { title: 'Test Page' });
 });
 
 module.exports = router;
