d0fbbaae1512a63e98e2be32c9f91b7eff74164b
Budmonde Duinkharjav <budmonde@gmail.com>
2015-09-19 13:37:37 -0400

    test page create

--- hackmit/routes/index.js
+++ hackmit/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '[Title of Project subject to change]' });
 });
 
+/* GET test page. */
+router.get('/test', function(req, res, next) {
+  res.render('test', { title: 'Test Page' });
+});
+
 module.exports = router;
