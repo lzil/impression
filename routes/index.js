07255a3a1b2a8552aae459168abb12e0e7902af4
Monde <budmonde@gmail.com>
2015-09-20 01:21:15 -0400

    liang da idiooot

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,8 @@ router.get('/test', function(req, res, next) {
 });
 
 router.get('/results', function(req, res, next) {
-    //var testImageURL = 'much-impression.herokuapp.com/images/img.jpg';
-    var imageurl = 'http://shoot4stars.herokuapp.com/images/lastpage.png'
+    //var testImageURL = 'much-impression.herokuapp.com/images`/img.jpg';
+    var testimageurl = 'http://shoot4stars.herokuapp.com/images/lastpage.png'
     console.log(imageurl)
     var ourId = "uploaded image";
 
