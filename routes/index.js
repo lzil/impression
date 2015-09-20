ecb48b53121a588ee008d75d4743454982ea0e92
Liang <zhoul@mit.edu>
2015-09-19 20:49:12 -0400

    clarifai

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs-extra'); 
+var fs = require('fs-extra');
+
+var clarifai = require('../clarifai_node.js');
+clarifai.initAPI("89yQDnMpTdF6VlNKDEzttY3ObL9xIiUYU1tZS1Z-", "AA6jlbyBXSfuw8y9cOoQYlfWNKvqf0g7GhjD-Ir5" );
+
 //var request = require('request');
 
 /* GET home page. */
@@ -14,6 +18,7 @@ router.get('/test', function(req, res, next) {
 });
 
 router.get('/results', function(req, res, next) {
+    clarifyCall();
     res.render('results', { title: 'Impression', searchUrl: 'http://www.clarifai.com/img/metro-north.jpg'});
 });
 
@@ -36,4 +41,12 @@ router.post('/upload', function (req, res, next) {
             });
         });
 });
+
+function clarifyCall() {
+    var testImageURL = 'https://www.petfinder.com/wp-content/uploads/2012/11/99233806-bringing-home-new-cat-632x475.jpg';
+    var ourId = "train station 1"; // this is any string that identifies the image to your system
+
+    clarifai.tagURL( testImageURL , ourId, function(err, res) {console.log(res.results[0].result.tag.classes)} );
+}
+
 module.exports = router;
